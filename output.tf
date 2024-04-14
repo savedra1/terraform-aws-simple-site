cb0a6be4b095edece4a1aeb34a8619289088883d
savedram <michael.savedra@ovo.com>
2024-04-14 13:15:17 +0100

    corrected output var

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 
 output "cloudfront_endpoint" {
-  value = module.cloudfront_without_domain[0].cloudfront_endpoint[0] ? var.domain_name != "" : module.cloudfront_with_domain[0].cloudfront_endpoint[0]
+  value = var.domain_name != "" ? module.cloudfront_with_domain[0].cloudfront_endpoint[0] : module.cloudfront_without_domain[0].cloudfront_endpoint[0]
 }
