abc5c27e4765480fa2dfdceaac1b92e27bfbe1a6
savedram <michael.savedra@ovo.com>
2024-05-20 15:22:05 +0100

    chore: update to always sign

--- modules/cloudfront_custom/oac.tf
+++ modules/cloudfront_custom/oac.tf
@@ -1,6 +1,6 @@
 resource "aws_cloudfront_origin_access_control" "oac" { 
   name                              = var.regional_domain
   origin_access_control_origin_type = "s3"
-  signing_behavior                  = "never"
+  signing_behavior                  = "always"
   signing_protocol                  = "sigv4"
 }
