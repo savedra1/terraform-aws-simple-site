0f242b82bfc880297c1577c01a93f0fc7ed6250e
Michael Savedra <99875823+savedra1@users.noreply.github.com>
2024-05-30 10:04:20 +0100

    chore: cleanup test code

--- provider.tf
+++ provider.tf
@@ -1,14 +1,5 @@
 # MODULE PROVIDERS
 
-# Backend setup
-terraform {
-  backend "s3" {
-    bucket = "tf-module-teststate-22132024"
-    key    = "dev/terraform.tfstate"
-    region = "eu-west-1"
-  }
-}
-
 provider "aws" { # Provider with input region (default)
   region = var.aws_region
 }
